a80c65d6ad30fdb1d5a2c6538acb4577bcc07997
Piya Srisuk <Piyamax23@gmail.com>
2021-02-15 22:16:24 +0700

    modified

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -11,6 +11,32 @@ function newGame() {
     }   
 }
 
+function checkWinCondition() {
+    if ((table[0]==='X' && table[1]==='X' && table[2]==='X')||
+        (table[3]==='X' && table[4]==='X' && table[5]==='X')||
+        (table[6]==='X' && table[7]==='X' && table[8]==='X')||
+        (table[0]==='X' && table[3]==='X' && table[6]==='X')||
+        (table[1]==='X' && table[4]==='X' && table[7]==='X')||
+        (table[2]==='X' && table[5]==='X' && table[8]==='X')||
+        (table[0]==='X' && table[4]==='X' && table[8]==='X')||
+        (table[2]==='X' && table[4]==='X' && table[6]==='X')) {
+        document.getElementById('result').innerHTML = 'You Win.' 
+        canClick = false;     
+        }
+    if ((table[0]==='O' && table[1]==='O' && table[2]==='O')||
+        (table[3]==='O' && table[4]==='O' && table[5]==='O')||
+        (table[6]==='O' && table[7]==='O' && table[8]==='O')||
+        (table[0]==='O' && table[3]==='O' && table[6]==='O')||
+        (table[1]==='O' && table[4]==='O' && table[7]==='O')||
+        (table[2]==='O' && table[5]==='O' && table[8]==='O')||
+        (table[0]==='O' && table[4]==='O' && table[8]==='O')||
+        (table[2]==='O' && table[4]==='O' && table[6]==='O')) {
+        document.getElementById('result').innerHTML = 'AI Win.';
+        canClick = false; 
+        return;     
+    }
+}
+
 function aiPlay () {
     let cell;
     do {
@@ -25,32 +51,10 @@ function humanPlay(cell) {
     if ((table[cell] === '')&&(canClick)) {
         table[cell] = humanPlayer;
         document.getElementById(cell).innerHTML = humanPlayer;
-        // chech win condition for Player
-        if ((table[0]==='X' && table[1]==='X' && table[2]==='X')||
-            (table[3]==='X' && table[4]==='X' && table[5]==='X')||
-            (table[6]==='X' && table[7]==='X' && table[8]==='X')||
-            (table[0]==='X' && table[3]==='X' && table[6]==='X')||
-            (table[1]==='X' && table[4]==='X' && table[7]==='X')||
-            (table[2]==='X' && table[5]==='X' && table[8]==='X')||
-            (table[0]==='X' && table[4]==='X' && table[8]==='X')||
-            (table[2]==='X' && table[4]==='X' && table[6]==='X')) {
-            document.getElementById('result').innerHTML = 'You Win.' 
-            canClick = false;     
-            return; 
-        }
-        aiPlay();
-        // chech win condition for AI
-        if ((table[0]==='O' && table[1]==='O' && table[2]==='O')||
-            (table[3]==='O' && table[4]==='O' && table[5]==='O')||
-            (table[6]==='O' && table[7]==='O' && table[8]==='O')||
-            (table[0]==='O' && table[3]==='O' && table[6]==='O')||
-            (table[1]==='O' && table[4]==='O' && table[7]==='O')||
-            (table[2]==='O' && table[5]==='O' && table[8]==='O')||
-            (table[0]==='O' && table[4]==='O' && table[8]==='O')||
-            (table[2]==='O' && table[4]==='O' && table[6]==='O')) {
-            document.getElementById('result').innerHTML = 'AI Win.';
-            canClick = false; 
-            return;     
+        checkWinCondition();
+        if (canClick) {
+            aiPlay();
+            checkWinCondition();
         }
     }
     if (table.indexOf('') === -1){

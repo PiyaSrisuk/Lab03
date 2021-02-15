01a055179d6c7b83f1d74e12765d6e935ab94871
Piya Srisuk <Piyamax23@gmail.com>
2021-02-15 11:48:45 +0700

    Finish

---
+++ JS Fav item/script.js
@@ -0,0 +1,4 @@
+let charactor = prompt('What is your favorit Charactor?');
+console.log('I like ' + charactor + '.');
+let color = prompt('What is the main color of ' + charactor + '.');
+console.log(charactor + ' is ' + color + '.');
---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,60 @@
+let humanPlayer = 'X', aiPlayer = 'O';
+let table = ['', '', '', '', '', '', '', '', ''];
+let canClick = true;
+
+function newGame() {
+    table = ['', '', '', '', '', '', '', '', ''];
+    canClick = true;
+    document.getElementById('result').innerHTML = 'Match result will show here.';
+    for (let i = 0; i < table.length; i++) {
+        document.getElementById(i).innerHTML = '';
+    }   
+}
+
+function aiPlay () {
+    let cell;
+    do {
+        if (table.indexOf('')===-1) return;
+        cell = Math.floor(Math.random() * 10) - 1;
+    }while(table[cell] !== '');
+    table[cell] = aiPlayer;
+    document.getElementById(cell).innerHTML = aiPlayer;
+}
+
+function humanPlay(cell) {
+    if ((table[cell] === '')&&(canClick)) {
+        table[cell] = humanPlayer;
+        document.getElementById(cell).innerHTML = humanPlayer;
+        // chech win condition for Player
+        if ((table[0]==='X' && table[1]==='X' && table[2]==='X')||
+            (table[3]==='X' && table[4]==='X' && table[5]==='X')||
+            (table[6]==='X' && table[7]==='X' && table[8]==='X')||
+            (table[0]==='X' && table[3]==='X' && table[6]==='X')||
+            (table[1]==='X' && table[4]==='X' && table[7]==='X')||
+            (table[2]==='X' && table[5]==='X' && table[8]==='X')||
+            (table[0]==='X' && table[4]==='X' && table[8]==='X')||
+            (table[2]==='X' && table[4]==='X' && table[6]==='X')) {
+            document.getElementById('result').innerHTML = 'You Win.' 
+            canClick = false;     
+            return; 
+        }
+        aiPlay();
+        // chech win condition for AI
+        if ((table[0]==='O' && table[1]==='O' && table[2]==='O')||
+            (table[3]==='O' && table[4]==='O' && table[5]==='O')||
+            (table[6]==='O' && table[7]==='O' && table[8]==='O')||
+            (table[0]==='O' && table[3]==='O' && table[6]==='O')||
+            (table[1]==='O' && table[4]==='O' && table[7]==='O')||
+            (table[2]==='O' && table[5]==='O' && table[8]==='O')||
+            (table[0]==='O' && table[4]==='O' && table[8]==='O')||
+            (table[2]==='O' && table[4]==='O' && table[6]==='O')) {
+            document.getElementById('result').innerHTML = 'AI Win.';
+            canClick = false; 
+            return;     
+        }
+    }
+    if (table.indexOf('') === -1){
+        canClick = false;
+        document.getElementById('result').innerHTML = 'Draw.';
+    }
+}
